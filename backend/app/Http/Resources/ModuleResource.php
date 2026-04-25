29b38f01ba9dc8a59df032276a638ba46d841f4e
mathyled <mathiasimagine@gmail.com>
2026-04-25 18:57:10 -0300

    feat: add ModuleResource for API responses

---
+++ backend/app/Http/Resources/ModuleResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ModuleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'course_id' => $this->course_id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'order' => $this->order,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}

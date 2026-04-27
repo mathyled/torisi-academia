7c8a36914e97cf47c84c742abdb39d7b48f3a1a4
mathyled <mathiasimagine@gmail.com>
2026-04-27 10:02:25 -0300

    feat(auth): register EnsureRole alias and return JSON 401 on unauthenticated requests

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Middleware\EnsureRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,8 +15,14 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'role' => EnsureRole::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->render(function (\Throwable $e, Request $request) {
+            if ($request->expectsJson() && $e instanceof \Illuminate\Auth\AuthenticationException) {
+                return response()->json(['message' => 'No autenticado.'], Response::HTTP_UNAUTHORIZED);
+            }
+        });
     })->create();
